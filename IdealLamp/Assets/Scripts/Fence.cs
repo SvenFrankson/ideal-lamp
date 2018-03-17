95c63740916f8532b87b6a1317a22b7e00063261
SvenFrankson <sven.frankson@gmail.com>
2018-03-17 14:34:15 +0100

    Rebuild scene. Initialize Fence path.

--- IdealLamp/Assets/Scripts/Fence.cs
+++ IdealLamp/Assets/Scripts/Fence.cs
@@ -13,10 +13,18 @@ public class Fence : MonoBehaviour {
 			return this._nodesContainer;
 		}
 	}
-	public Vector3[] path;
+	public List<Vector3> path;
 
 	public void Start() {
-		this.CreateMesh();
+		this.InitializePath();
+		this.UpdateMesh();
+	}
+
+	public void InitializePath() {
+		int count = this.nodesContainer.childCount;
+		for (int i = 0; i < count; i++) {
+			this.path.Add(this.nodesContainer.GetChild(i).transform.position);
+		}
 	}
 
 	public void UpdateMesh() {
@@ -25,9 +33,9 @@ public class Fence : MonoBehaviour {
 	}
 
 	public void CreateMesh() {
-		for (int i = 0; i < this.path.Length; i++) {
+		for (int i = 0; i < this.path.Count; i++) {
 			Vector3 A = this.path[i];
-			Vector3 B = this.path[(i + 1) % this.path.Length];
+			Vector3 B = this.path[(i + 1) % this.path.Count];
 			GameObject fencePart = GameObject.CreatePrimitive(PrimitiveType.Cube);
 			fencePart.transform.position = (B + A) * 0.5f;
 			fencePart.transform.rotation = Quaternion.FromToRotation(
@@ -44,18 +52,17 @@ public class Fence : MonoBehaviour {
 	}
 
 	public void ClearMesh() {
-		Transform child = this.transform.GetChild(0);
-		while (child != null) {
-			Destroy(child);
-			child = this.transform.GetChild(0);
+		int count = this.nodesContainer.childCount;
+		for (int i = 0; i < count; i++) {
+			Destroy(this.nodesContainer.GetChild(i).gameObject);
 		}
 	}
 
 	public float Length() {
 		float l = 0f;
-		for (int i = 0; i < this.path.Length; i++) {
+		for (int i = 0; i < this.path.Count; i++) {
 			Vector3 A = this.path[i];
-			Vector3 B = this.path[(i + 1) % this.path.Length];
+			Vector3 B = this.path[(i + 1) % this.path.Count];
 			l += Vector3.Distance(A, B);
 		}
 		return l;
@@ -73,7 +80,7 @@ public class Fence : MonoBehaviour {
 		int index = this.FencePosToNodeIndex(fencePos, out tmpL);
 		float deltaPos = fencePos - tmpL;
 		Vector3 A = this.path[index];
-		Vector3 B = this.path[(index + 1) % this.path.Length];
+		Vector3 B = this.path[(index + 1) % this.path.Count];
 		return this.path[index] + (B - A).normalized * deltaPos;
 	}
 
@@ -86,7 +93,7 @@ public class Fence : MonoBehaviour {
 		while (tmpL + d < fencePos) {
 			index ++;
 			A = this.path[index];
-			B = this.path[(index + 1) % this.path.Length];
+			B = this.path[(index + 1) % this.path.Count];
 			tmpL += d;
 			d = Vector3.Distance(A, B);
 		}
