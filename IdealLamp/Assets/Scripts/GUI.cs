da1da8b5c237bede5319d38f91b5b7bbb95633a9
SvenFrankson <sven.frankson@gmail.com>
2018-04-05 00:50:21 +0200

    Add dummy GUI.

---
+++ IdealLamp/Assets/Scripts/GUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI : MonoBehaviour {
+
+	public void OnGUI() {
+		GUILayout.BeginHorizontal();
+		GUILayout.TextArea("Time " + Time.timeSinceLevelLoad.ToString("0.0"));
+		GUILayout.TextArea("Chickens " + Chicken.instances.Count);
+		GUILayout.TextArea("Dogs " + Dog.instances.Count);
+		GUILayout.EndHorizontal();
+	}
+}
